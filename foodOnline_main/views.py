8c955ead81ea56412c53b72abd1f003a2c4aa6b1
unknown <ssghodekar1997@gmail.com>
2023-08-17 11:00:46 +0530

    homepage setup,postgres configuration

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello world")
+    return render(request,'home.html')
